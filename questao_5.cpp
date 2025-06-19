37cdb8fc252370e3e8c38ede97e69ceb6c4e52bb
Karim Soares Lentz <karim.lentz25@gmail.com>
2025-06-19 15:04:05 -0300

    questao_5.cpp

--- questao_5.cpp
+++ questao_5.cpp
@@ -7,13 +7,13 @@ int main(){
 	int a, b, c, d;
 	cin >> a >> b >> c >> d;
 	
-	if ( d < a){
+	if ( d <= a){
 		cout << d << endl << a << endl << b << endl << c << endl;
-	} else if ( d > a && d < b){
+	} else if ( d > a && d <= b){
 		cout << a << endl << d << endl << b << endl << c << endl;
-	} else if ( d > b && d < c){
+	} else if ( d > b && d <= c){
 		cout << a << endl << b << endl << d << endl << c << endl;
-	} else if ( d > c){
+	} else if ( d >= c){
 		cout << a << endl << b << endl << c << endl << d << endl;
 	}		
 		
